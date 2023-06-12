67bed5a6d7a30c100149d201d1be8dee7e1c3d22
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2023-06-12 19:30:45 +0900

    Change bird frame color

--- yolo_with_depth.py
+++ yolo_with_depth.py
@@ -12,15 +12,13 @@ import blobconverter
 
 MAX_Z = 15000
 DISPLAY_WINDOW_SIZE_RATE = 3.0
-
-
+idColors = np.random.random(size=(256, 3)) * 256
 class TextHelper:
     def __init__(self) -> None:
         self.bg_color = (0, 0, 0)
         self.color = (255, 255, 255)
         self.text_type = cv2.FONT_HERSHEY_SIMPLEX
         self.line_type = cv2.LINE_AA
-        self._bboxColors = np.random.random(size=(256, 3)) * 256
 
     def putText(self, frame, text, coords):
         cv2.putText(frame, text, coords, self.text_type,
@@ -30,7 +28,7 @@ class TextHelper:
 
     def rectangle(self, frame, p1, p2, id):
         cv2.rectangle(frame, p1, p2, (0, 0, 0), 4)
-        cv2.rectangle(frame, p1, p2, self._bboxColors[id], 2)
+        cv2.rectangle(frame, p1, p2, idColors[id], 2)
 
 
 class HostSync:
@@ -82,11 +80,13 @@ def draw_bird_frame(frame, x, z, id=None):
     pointY = frame.shape[0] - int(z / (MAX_Z - 10000) * frame.shape[0]) - 20
     pointX = int(-x / max_x * frame.shape[1] + frame.shape[1] / 2)
     if id is not None:
-        cv2.putText(frame, str(id), (pointX - 30, pointY + 5),
-                    cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 255, 0))
-    cv2.circle(frame, (pointX, pointY), 2, (0, 255, 0),
-               thickness=5, lineType=8, shift=0)
-
+        #cv2.putText(frame, str(id), (pointX - 30, pointY + 5),
+        #           cv2.FONT_HERSHEY_TRIPLEX, 0.5, (0, 255, 0))
+        cv2.circle(frame, (pointX, pointY), 2, idColors[id],
+                    thickness=5, lineType=8, shift=0)
+    else:
+        cv2.circle(frame, (pointX, pointY), 2, (0, 255, 0),
+            thickness=5, lineType=8, shift=0)
 
 def frameNorm(frame, bbox):
     normVals = np.full(len(bbox), frame.shape[0])
@@ -144,7 +144,6 @@ def main() -> None:
             args.model, shaves=6, zoo_type="depthai", use_cache=True))
 
     syncNN = False
-
     # Create pipeline
     pipeline = dai.Pipeline()
 
@@ -251,7 +250,6 @@ def main() -> None:
                 birds = birdseyeframe.copy()
                 detections = msgs["detections"].detections
                 frame = msgs["rgb"].getCvFrame()
-                cv2.imshow("rgb", frame)
                 depthFrame = msgs["depth"].getFrame()
                 depthFrameColor = cv2.normalize(
                     depthFrame, None, 256, 0, cv2.NORM_INF, cv2.CV_8UC3)
@@ -302,7 +300,7 @@ def main() -> None:
                             detection.spatialCoordinates.z / 1000), (x2 + 10, y1 + 100))
 
                     draw_bird_frame(birds, detection.spatialCoordinates.x,
-                                    detection.spatialCoordinates.z)
+                                    detection.spatialCoordinates.z, detection.label)
 
             if display is not None:
                 # Birdseye view
