667a258a08b0dfc20a1a8d556e4351e020be4159
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2023-09-02 23:01:04 +0900

    fix tracking disp

--- oakd_yolo/oakd_tracking_yolo.py
+++ oakd_yolo/oakd_tracking_yolo.py
@@ -313,41 +313,41 @@ class OakdTrackingYolo(object):
             brank_height = width - height
             if tracklets is not None:
                 for tracklet in tracklets:
-                    # if tracklet.status.name == "TRACKED":
-                    roi = tracklet.roi.denormalize(frame.shape[1], frame.shape[0])
-                    x1 = int(roi.topLeft().x)
-                    y1 = int(roi.topLeft().y)
-                    x2 = int(roi.bottomRight().x)
-                    y2 = int(roi.bottomRight().y)
-                    try:
-                        label = self.labels[tracklet.label]
-                    except:
-                        label = tracklet.label
-                    self.text.put_text(frame, str(label), (x1 + 10, y1 + 20))
-                    self.text.put_text(
-                        frame,
-                        f"ID: {[tracklet.id]}",
-                        (x1 + 10, y1 + 45),
-                    )
-                    self.text.put_text(frame, tracklet.status.name, (x1 + 10, y1 + 70))
+                    if tracklet.status.name == "TRACKED":
+                        roi = tracklet.roi.denormalize(frame.shape[1], frame.shape[0])
+                        x1 = int(roi.topLeft().x)
+                        y1 = int(roi.topLeft().y)
+                        x2 = int(roi.bottomRight().x)
+                        y2 = int(roi.bottomRight().y)
+                        try:
+                            label = self.labels[tracklet.label]
+                        except:
+                            label = tracklet.label
+                        self.text.put_text(frame, str(label), (x1 + 10, y1 + 20))
+                        self.text.put_text(
+                            frame,
+                            f"ID: {[tracklet.id]}",
+                            (x1 + 10, y1 + 45),
+                        )
+                        self.text.put_text(frame, tracklet.status.name, (x1 + 10, y1 + 70))
 
-                    self.text.rectangle(frame, (x1, y1), (x2, y2), tracklet.id)
-                    if tracklet.spatialCoordinates.z != 0:
-                        self.text.put_text(
-                            frame,
-                            "X: {:.2f} m".format(tracklet.spatialCoordinates.x / 1000),
-                            (x1 + 10, y1 + 95),
-                        )
-                        self.text.put_text(
-                            frame,
-                            "Y: {:.2f} m".format(tracklet.spatialCoordinates.y / 1000),
-                            (x1 + 10, y1 + 120),
-                        )
-                        self.text.put_text(
-                            frame,
-                            "Z: {:.2f} m".format(tracklet.spatialCoordinates.z / 1000),
-                            (x1 + 10, y1 + 145),
-                        )
+                        self.text.rectangle(frame, (x1, y1), (x2, y2), tracklet.id)
+                        if tracklet.spatialCoordinates.z != 0:
+                            self.text.put_text(
+                                frame,
+                                "X: {:.2f} m".format(tracklet.spatialCoordinates.x / 1000),
+                                (x1 + 10, y1 + 95),
+                            )
+                            self.text.put_text(
+                                frame,
+                                "Y: {:.2f} m".format(tracklet.spatialCoordinates.y / 1000),
+                                (x1 + 10, y1 + 120),
+                            )
+                            self.text.put_text(
+                                frame,
+                                "Z: {:.2f} m".format(tracklet.spatialCoordinates.z / 1000),
+                                (x1 + 10, y1 + 145),
+                            )
             if birds:
                 self.draw_bird_frame(tracklets)
             cv2.putText(
